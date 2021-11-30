e7ae5c5fb57e7dff569da6c395bbe68fbd744613
Bernie Bernstein <bernard@thebernsteins.com>
2021-11-30 10:59:18 -0500

    Add Native Picker and test for it

--- e2e/e2e.test.js
+++ e2e/e2e.test.js
@@ -1,14 +1,38 @@
-describe('DatePicker', () => {
+describe('CompareDatePickers', () => {
     beforeAll(async () => {
         await page.goto('http://localhost:3000');
     });
 
-    it('should get a date', async () => {
-        const dateToEnter = '01/01/2011';
-        const selector = 'input';
-        await page.click(selector, { clickCount: 3 });
-        await page.type(selector, dateToEnter);
-        const dateEntered = await page.$eval(selector, el => el.value);
-        expect(dateEntered).toEqual(dateToEnter);
-    })
+    const isoDate = '2011-01-01';
+    const localeDate = '01/01/2011';
+
+    describe('DatePicker', () => {
+        it('should get a date from a DatePicker', async () => {
+            const selector = '#datePicker';
+            await page.click(selector);
+//            await page.click(selector, { clickCount: 3 });
+            for (let i=0; i<10; i++) {
+                await page.keyboard.press('ArrowLeft');
+            }
+            await page.type(selector, localeDate);
+            const dateEntered = await page.$eval(selector, el => el.value);
+            expect(dateEntered).toEqual(localeDate);
+        })
+    });
+
+    describe('NativePicker', () => {
+        it('should get a date from Native Picker', async () => {
+            const selector = '#nativePicker';
+            await page.click(selector);
+            for (let i=0; i<3; i++) {
+                await page.keyboard.press('ArrowLeft');
+            }
+            await page.type(selector, localeDate);
+            const dateInForm = await page.$eval(selector, el => el.value);
+            expect(dateInForm).toEqual(isoDate);
+        })
+
+    });
+
+
 });
