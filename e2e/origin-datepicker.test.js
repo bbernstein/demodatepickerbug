817bf70852c8cb4ca93854cfdb874d7105326031
Bernie Bernstein <bernard@thebernsteins.com>
2021-12-03 18:03:06 -0500

    Add test of original page

---
+++ e2e/origin-datepicker.test.js
@@ -0,0 +1,46 @@
+describe('CompareDatePickers', () => {
+    beforeAll(async () => {
+        await page.goto('https://mui.com/components/date-picker/');
+    }, 10000);
+
+    const localeDate = '01/01/2011';
+
+    describe('DatePicker', () => {
+        it('should get a date from a DatePicker', async () => {
+            const selector = '#mui-36';
+
+            // tried this several ways with same result
+            // triple-click selects the whole field
+            // await page.click(selector, {clickCount: 3});
+
+            // click and then left-arrow the longest length of a date
+            await page.click(selector);
+            for (let i = 0; i < 10; i++) {
+                await page.keyboard.press('ArrowLeft');
+            }
+
+            await page.type(selector, localeDate);
+            const dateEntered = await page.$eval(selector, el => el.value);
+            expect(dateEntered).toEqual(localeDate);
+        })
+    });
+
+    // describe('NativePicker', () => {
+    //     it('should get a date from Native Picker', async () => {
+    //         const selector = '#nativePicker';
+    //
+    //         // in native picker, there are three fields to back up through at most
+    //         await page.click(selector);
+    //         for (let i = 0; i < 3; i++) {
+    //             await page.keyboard.press('ArrowLeft');
+    //         }
+    //
+    //         await page.type(selector, localeDate);
+    //         const dateInForm = await page.$eval(selector, el => el.value);
+    //         expect(dateInForm).toEqual(isoDate);
+    //     })
+    //
+    // });
+
+
+});
